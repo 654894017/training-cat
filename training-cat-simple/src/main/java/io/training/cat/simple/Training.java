e8af2a34868be940f11cbf127dadb9556e936aa6
Percy0601 <2008dreams@2008.sina.com>
2016-03-17 22:42:53 -0700

    可以使用了

--- training-cat-simple/src/main/java/io/training/cat/simple/Training.java
+++ training-cat-simple/src/main/java/io/training/cat/simple/Training.java
@@ -1,24 +1,29 @@
 package io.training.cat.simple;
 
 import com.dianping.cat.Cat;
+import com.dianping.cat.message.Message;
 import com.dianping.cat.message.Transaction;
 
 public class Training {
 	
 	public static void main(String[] args) {
+		int i = 0;
 		while(true) {
 			Transaction t = null;
 			try {
-				t = Cat.getProducer().newTransaction("URL", "/helloworld");
+				t = Cat.getProducer().newTransaction("URL====", "/helloworld");
 				System.out.println("======================");
-				t.setStatus(Transaction.SUCCESS);
+				t.setStatus(Message.SUCCESS);
+				t.addData("============vadf");
+				Cat.getProducer().newEvent("Training", i++ + "======");
 				Thread.sleep(2000);
+				t.complete();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
-				t.complete();
+				
 			}
 			
 		}
