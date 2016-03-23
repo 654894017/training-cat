ba1a30583c2aef97d40335d471d080b7b97024d8
Percy0601 <2008dreams@2008.sina.com>
2016-03-23 20:36:40 +0800

    成功完全所有配置和代码

--- training-cat-dubbo/training-cat-dubbo-consumer/src/main/java/io/training/cat/dubbo/consumer/controller/TrainingController.java
+++ training-cat-dubbo/training-cat-dubbo-consumer/src/main/java/io/training/cat/dubbo/consumer/controller/TrainingController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.dianping.cat.Cat;
+import com.dianping.cat.CatConstants;
+import com.dianping.cat.message.Event;
+import com.dianping.cat.message.Transaction;
+import com.dianping.cat.message.spi.MessageTree;
 
 @RestController
 public class TrainingController {
@@ -15,8 +21,31 @@ public class TrainingController {
 	
 	@RequestMapping("/hello")
 	public String hello(String name) {
+		Transaction t = Cat.newTransaction("URL", "/hello");
+		String serverMessageId = Cat.getProducer().createMessageId();
+		MessageTree tree = Cat.getManager().getThreadLocalMessageTree();
+		if (null == tree) {
+			Cat.setup(null);
+			tree = Cat.getManager().getThreadLocalMessageTree();
+		}
+		
+		String rootMessageId = tree.getRootMessageId() == null ? tree.getMessageId() : tree.getRootMessageId();
+		String currentMessageId = tree.getMessageId();
+		
+		// 传递上下文
+		RpcContext.getContext().setAttachment(CatConstants.PIGEON_ROOT_MESSAGE_ID, rootMessageId);
+		RpcContext.getContext().setAttachment(CatConstants.PIGEON_CURRENT_MESSAGE_ID, currentMessageId);
+		RpcContext.getContext().setAttachment(CatConstants.PIGEON_SERVER_MESSAGE_ID, serverMessageId);
+		Cat.logEvent(CatConstants.TYPE_REMOTE_CALL, CatConstants.NAME_REQUEST, Transaction.SUCCESS, serverMessageId);
+		Transaction t1 = Cat.newTransaction("调用远程Dubbo服务", "findAll");
 		System.out.println("==========" +  (userService == null));
-		return "Hello: " + name + userService.findAll().size();
+		String result = "Hello: " + name + userService.findAll().size();
+		t1.setStatus(Transaction.SUCCESS);
+		t1.complete();
+		Cat.logEvent("Call", "DubboServer", Event.SUCCESS, "parentId:" + serverMessageId+ "|msgId:" +  serverMessageId + " End...");
+		t.setStatus(Transaction.SUCCESS);
+		t.complete();
+		return result;
 	}
 
 }
--- training-cat-dubbo/training-cat-dubbo-provider/src/main/java/io/training/cat/dubbo/provider/dubbo/service/UserServiceImpl.java
+++ training-cat-dubbo/training-cat-dubbo-provider/src/main/java/io/training/cat/dubbo/provider/dubbo/service/UserServiceImpl.java
@@ -7,11 +7,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.dianping.cat.Cat;
+import com.dianping.cat.CatConstants;
+import com.dianping.cat.message.Event;
+import com.dianping.cat.message.Transaction;
+import com.dianping.cat.message.spi.MessageTree;
 
 @Service(protocol = { "dubbo" })
 public class UserServiceImpl implements UserService {
 	@Override
 	public List<User> findAll() {
+		String rootMessageId = RpcContext.getContext().getAttachment(CatConstants.PIGEON_ROOT_MESSAGE_ID);
+		String currentMessageId = RpcContext.getContext().getAttachment(CatConstants.PIGEON_CURRENT_MESSAGE_ID);
+		String serverMessageId = RpcContext.getContext().getAttachment(CatConstants.PIGEON_SERVER_MESSAGE_ID);
+		Transaction t = Cat.newTransaction("RemoteCall", "findAll");
+		
+		MessageTree tree = Cat.getManager().getThreadLocalMessageTree();
+		if (null == tree) {
+			Cat.setup(null);
+			tree = Cat.getManager().getThreadLocalMessageTree();
+		}
+		
+		tree.setRootMessageId(rootMessageId);
+		tree.setParentMessageId(serverMessageId);
+		tree.setMessageId(currentMessageId);
+		
 		List<User> users = new ArrayList<User>();
 		int i = 0;
 		
@@ -22,6 +43,9 @@ public class UserServiceImpl implements UserService {
 			users.add(u);
 		}
 		
+		Cat.logEvent("Call", "Params", Event.SUCCESS, "parentId:" + serverMessageId + "|msgId:" + currentMessageId + "End...");
+		t.setStatus(Transaction.SUCCESS);
+		t.complete();
 		return users;
 	}
 
